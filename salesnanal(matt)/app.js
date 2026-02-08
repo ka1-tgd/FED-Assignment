72826cf10d20a4dbc6ee00893072096603a0c55a
maddias-np <s10273131@connect.np.edu.sg>
2026-02-08 23:48:22 +0800

    added comments

--- salesnanal(matt)/app.js
+++ salesnanal(matt)/app.js
@@ -1,15 +1,15 @@
 // Highlights the active nav link automatically based on file name.
 (function () {
-  const path = window.location.pathname.split("/").pop() || "index.html"; //extraxcts name of current page url
-  const links = document.querySelectorAll(".nav a"); //loop all nav links
+  const path = window.location.pathname.split("/").pop() || "index.html"; //extracts name of current page url
+  const links = document.querySelectorAll(".nav a"); //grabs all <a> tags with class .nav and returns a NodeList  
 
-  links.forEach(a => {
+  links.forEach(a => { //loops through each nav link
     const href = a.getAttribute("href");
     if (href === path) a.classList.add("active"); //if href is the filename, add class active. then draws underline
   });
 
   
-  const logout = document.querySelector('[data-logout="true"]');
+  const logout = document.querySelector('[data-logout="true"]'); //finds log out link
   if (logout) {
     logout.addEventListener("click", (e) => {
       e.preventDefault();
